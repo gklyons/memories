1720dcbbb74a63c0a6b75ceef907ee23b3241125
Caleb Strong <hahnstrong@gmail.com>
2017-09-20 12:27:57 -0600

    Started GroupVCs storyboard

---
+++ Memories/Memories/GroupViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GroupViewController.swift
+//  Memories
+//
+//  Created by Caleb Strong on 9/20/17.
+//  Copyright © 2017 Garrett Lyons. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var groupTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        groupTableView.delegate = self
+        groupTableView.dataSource = self
+    }
+    
+    // MARK: - Table View Data Source Functions
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
---
+++ Memories/Memories/SelectPersonTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SelectPersonTableViewCell.swift
+//  Memories
+//
+//  Created by Caleb Strong on 9/20/17.
+//  Copyright © 2017 Garrett Lyons. All rights reserved.
+//
+
+import UIKit
+
+class SelectPersonTableViewCell: UITableViewCell {
+
+}
