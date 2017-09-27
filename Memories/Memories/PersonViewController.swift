0b16466633ddefb3b7bdbc663b4bc12a7f8c1d1e
gklyons <30358106+gklyons@users.noreply.github.com>
2017-09-27 16:28:45 -0600

    Fixed merge conflic

--- Memories/Memories/PersonViewController.swift
+++ Memories/Memories/PersonViewController.swift
@@ -15,23 +15,19 @@ class PersonViewController: UIViewController, UITextFieldDelegate, UINavigationC
     
     var imagePicker = UIImagePickerController()
     var person: Person?
-<<<<<<< HEAD
+
     var image: UIImage?
-=======
->>>>>>> developLogan
-    
+
     // MARK: - IBOutlets
     
     @IBOutlet weak var memoryListTableView: UITableView!
     
     // MARK: - IBActions
     
-<<<<<<< HEAD
+
     @objc func newMemoryButtonTapped() {
         self.performSegue(withIdentifier: "ToMemoryVC", sender: self)
-=======
-    
-    
+    }
     @IBAction func personPhotoButtonTapped(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) {
             print("Button capture")
@@ -42,7 +38,7 @@ class PersonViewController: UIViewController, UITextFieldDelegate, UINavigationC
             
             self.present(imagePicker, animated:  true, completion: nil)
         }
->>>>>>> developLogan
+
     }
     
     override func viewDidLoad() {
