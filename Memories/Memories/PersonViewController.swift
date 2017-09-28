8a8e56c558c143d58421dca8bfd9ba1c82dde002
Logan Davis <logandavis@Logans-MacBook-Pro.local>
2017-09-28 13:42:36 -0600

    Added photos and launch storyboard

--- Memories/Memories/PersonViewController.swift
+++ Memories/Memories/PersonViewController.swift
@@ -15,10 +15,8 @@ class PersonViewController: UIViewController, UITextFieldDelegate, UINavigationC
     
     var imagePicker = UIImagePickerController()
     var person: Person?
-<<<<<<< HEAD
+
     var image: UIImage?
-=======
->>>>>>> developLogan
     
     // MARK: - IBOutlets
     
@@ -26,10 +24,10 @@ class PersonViewController: UIViewController, UITextFieldDelegate, UINavigationC
     
     // MARK: - IBActions
     
-<<<<<<< HEAD
+
     @objc func newMemoryButtonTapped() {
         self.performSegue(withIdentifier: "ToMemoryVC", sender: self)
-=======
+    }
     
     
     @IBAction func personPhotoButtonTapped(_ sender: Any) {
@@ -42,7 +40,6 @@ class PersonViewController: UIViewController, UITextFieldDelegate, UINavigationC
             
             self.present(imagePicker, animated:  true, completion: nil)
         }
->>>>>>> developLogan
     }
     
     override func viewDidLoad() {
